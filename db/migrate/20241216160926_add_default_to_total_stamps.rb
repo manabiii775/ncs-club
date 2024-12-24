810c8ae85356b567d003ddeb1c1cb102dfd18f05
manabiii775 <ke218.nnkdal@icloud.com>
2024-12-24 22:37:58 +0900

    マイグレーションファイルの編集再

--- db/migrate/20241216160926_add_default_to_total_stamps.rb
+++ db/migrate/20241216160926_add_default_to_total_stamps.rb
@@ -1,5 +1,5 @@
 class AddDefaultToTotalStamps < ActiveRecord::Migration[7.0]
   def change
-    change_column_default :stampcards, :total_stamps, from: nil, 0
+    change_column_default :stampcards, :total_stamps, from: nil, to: 0
   end
 end
